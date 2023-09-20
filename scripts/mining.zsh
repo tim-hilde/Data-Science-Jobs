718a1bce850ab901bd61129d33d4bbfb89d7df07
tim-hilde <tim.hildebrandt@me.com>
2023-09-20 07:38:28 +0200

    move mining script

--- scripts/mining.zsh
+++ scripts/mining.zsh
@@ -1,10 +1,10 @@
 #!/bin/zsh
 
+python3 "./scraping.py" 
+
+python3 "./add_info.py"
+
 DATUM=$(date +"%d.%m.%Y")
 git add .
 git commit -a -m "$DATUM Scraping"
 git push
-
-# python3 "./scraping.py" 
-
-# python3 "./add_info.py"
