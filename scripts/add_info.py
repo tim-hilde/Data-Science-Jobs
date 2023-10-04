127d05e83cbbc78aea6d39e14b6d353b150798d9
tim-hilde <tim.hildebrandt@me.com>
2023-10-04 11:57:45 +0200

    04.10.2023 Scraping

--- scripts/add_info.py
+++ scripts/add_info.py
@@ -6,84 +6,143 @@ from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
+# Konfigurieren des Firefox-Browsers
 options = FirefoxOptions()
 options.add_argument("--headless")
 driver = webdriver.Firefox(options=options)
 # driver = webdriver.Firefox()
-
 first = True
+
+# Funktion zum Hinzufügen von Informationen zu einem Job-Link
 def add_info(url):
-	global first
-	driver.get(url)
-	wait = WebDriverWait(driver, 20)
-	if "gehalt.de" in url:
-		wait.until(lambda driver: driver.current_url != url)
-	link = driver.current_url
-	if "stepstone.de" not in link:
-		return(link, 
-				"Nicht stepstone",
-				"Nicht stepstone",
-				"Nicht stepstone",
-				"Nicht stepstone",
-				"Nicht stepstone",
-				"Nicht stepstone",)
-	# print(link)
-	if first:
-		wait.until(lambda driver: len(driver.find_elements(By.CSS_SELECTOR, "#ccmgt_explicit_preferences.privacy-prompt-button.secondary-button.options-button")) > 0)
-		driver.find_element(By.CSS_SELECTOR, "#ccmgt_explicit_preferences.privacy-prompt-button.secondary-button.options-button").click()
-		driver.find_element(By.CSS_SELECTOR, ".secondary-button.ccmgt_reject_button").click()
-		first = False
+    global first
+    driver.get(url)
+    wait = WebDriverWait(driver, 20)
+    
+    # Überprüfen, ob die URL von gehalt.de ist
+    if "gehalt.de" in url:
+        wait.until(lambda driver: driver.current_url != url)
+    link = driver.current_url
+    if "stepstone.de" not in link:
+        return (
+            link,
+            "Nicht stepstone",
+            "Nicht stepstone",
+            "Nicht stepstone",
+            "Nicht stepstone",
+            "Nicht stepstone",
+            "Nicht stepstone",
+        )
+    
+	# Beim ersten Durchlauf, Cookie Banner schließen
+    if first:
+        # Sicherstellen, dass es einen Cookie-Banner gibt
+        wait.until(
+            lambda driver: len(
+                driver.find_elements(
+                    By.CSS_SELECTOR,
+                    "#ccmgt_explicit_preferences.privacy-prompt-button.secondary-button.options-button",
+                )
+            )
+            > 0
+        )
+        # Klicke ersten Teil des Cookie Banners
+        driver.find_element(
+            By.CSS_SELECTOR,
+            "#ccmgt_explicit_preferences.privacy-prompt-button.secondary-button.options-button",
+        ).click()
+        # Klicke zweiten Teil des Cookie Banners
+        wait.until(
+            lambda driver: len(
+                driver.find_elements(
+                    By.CSS_SELECTOR,
+                    ".secondary-button.ccmgt_reject_button",
+                )
+            )
+            > 0
+        )
+        driver.find_element(
+            By.CSS_SELECTOR, ".secondary-button.ccmgt_reject_button"
+        ).click()
+        first = False
+	# Überprüfen, dass alle Elemente des Texts verfügbar sind
+    if len(driver.find_elements(By.CLASS_NAME, "listing-content-provider-hw0nf9")) > 0:
+        driver.find_element(By.CLASS_NAME, "listing-content-provider-hw0nf9").click()
 
-	if len(driver.find_elements(By.CLASS_NAME, "listing-content-provider-hw0nf9")) > 0:
-		driver.find_element(By.CLASS_NAME, "listing-content-provider-hw0nf9").click()
-	
-	response = driver.page_source
-	soup_object = BeautifulSoup(response, features="lxml")
-	if len(soup_object.find_all(attrs={"data-genesis-element":"ALERT_CONTENT"})) > 0:
-		return(link, 
-				"Stellenanzeige nicht mehr verfügbar", 
-				"Stellenanzeige nicht mehr verfügbar", 
-				"Stellenanzeige nicht mehr verfügbar", 
-				"Stellenanzeige nicht mehr verfügbar", 
-				"Stellenanzeige nicht mehr verfügbar", 
-				"Stellenanzeige nicht mehr verfügbar")
-	# Teilzeit und Remote
-	if len(soup_object.find_all(class_="at-listing__list-icons_work-type")) > 0:
-		work_type = soup_object.find(class_="at-listing__list-icons_work-type").find(attrs={"data-genesis-element":"TEXT"})
-		# # print(soup_object)
-		# print(work_type)
-		teilzeit_remote = work_type.text
-	else:
-		teilzeit_remote = "-"
+    response = driver.page_source
+    soup_object = BeautifulSoup(response, features="lxml")
+    # Wenn Stellenanzeige nicht mehr verfügbar ist
+    if len(soup_object.find_all(attrs={"data-genesis-element": "ALERT_CONTENT"})) > 0:
+        return (
+            link,
+            "Stellenanzeige nicht mehr verfügbar",
+            "Stellenanzeige nicht mehr verfügbar",
+            "Stellenanzeige nicht mehr verfügbar",
+            "Stellenanzeige nicht mehr verfügbar",
+            "Stellenanzeige nicht mehr verfügbar",
+            "Stellenanzeige nicht mehr verfügbar",
+        )
+    # Teilzeit und Remote
+    if len(soup_object.find_all(class_="at-listing__list-icons_work-type")) > 0:
+        work_type = soup_object.find(class_="at-listing__list-icons_work-type").find(
+            attrs={"data-genesis-element": "TEXT"}
+        )
+        teilzeit_remote = work_type.text
+    else:
+        teilzeit_remote = "-"
 
-	base = soup_object.find_all("div", class_="sc-EHOje jWEhHL")
+	# Informationstexte auslesen
+    base = soup_object.find_all("div", class_="sc-EHOje jWEhHL")
 
-	texts = ["", "", "", "", ""]
+    texts = ["", "", "", "", ""]
 
-	for i in range(len(base)):
-		area = base[i].find_all(["p", "li"])
-		for element in area:
-			texts[i] += element.text + "\n"
+    for i in range(len(base)):
+        area = base[i].find_all(["p", "li"])
+        for element in area:
+            texts[i] += element.text
 
-	return link, teilzeit_remote, texts[0], texts[1], texts[2], texts[3], texts[4]
+    return link, teilzeit_remote, texts[0], texts[1], texts[2], texts[3], texts[4]
+
 
 jobs = pd.read_pickle("../data/jobs.pkl")
 
-jobs_new = jobs[jobs["Teilzeit_Remote"].isna() == True]
+jobs_new = jobs[jobs["Teilzeit_Remote"].isna()]
 index_new = jobs_new.index
 for i in index_new:
-	jobs.loc[i, ["Link", "Teilzeit_Remote", "Introduction", "Description", "Profile", "We_offer", "Contact"]] = add_info(jobs_new.loc[i, "Link"])
-	jobs.to_pickle("../data/jobs.pkl")
+    jobs.loc[
+        i,
+        [
+            "Link",
+            "Teilzeit_Remote",
+            "Introduction",
+            "Description",
+            "Profile",
+            "We_offer",
+            "Contact",
+        ],
+    ] = add_info(jobs_new.loc[i, "Link"])
+    jobs.to_pickle("../data/jobs.pkl")
 
 jobs_error = jobs[(jobs["Teilzeit_Remote"] == "-") & (jobs["Introduction"] == "")]
 
 round = 0
 while len(jobs_error) > 0 and round < 3:
-	index_err = jobs_error.index
-	for i in index_err:
-			jobs.loc[i, ["Link", "Teilzeit_Remote", "Introduction", "Description", "Profile", "We_offer", "Contact"]] = add_info(jobs_error.loc[i, "Link"])
-	jobs_error = jobs[(jobs["Teilzeit_Remote"] == "-") & (jobs["Introduction"] == "")]
-	jobs.to_pickle("../data/jobs.pkl")
-	round +=1
+    index_err = jobs_error.index
+    for i in index_err:
+        jobs.loc[
+            i,
+            [
+                "Link",
+                "Teilzeit_Remote",
+                "Introduction",
+                "Description",
+                "Profile",
+                "We_offer",
+                "Contact",
+            ],
+        ] = add_info(jobs_error.loc[i, "Link"])
+    jobs_error = jobs[(jobs["Teilzeit_Remote"] == "-") & (jobs["Introduction"] == "")]
+    jobs.to_pickle("../data/jobs.pkl")
+    round += 1
 
 driver.quit()
--- scripts/scraping.py
+++ scripts/scraping.py
@@ -7,9 +7,15 @@ from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-search_keywords = ["junior+data+scientist", "junior+data+science", "junior+data+analyst", "junior+data+analysis"]
+search_keywords = [
+    "junior+data+scientist",
+    "junior+data+science",
+    "junior+data+analyst",
+    "junior+data+analysis",
+]
 search_url = "https://www.gehalt.de/einkommen/suche/"
 
+# Konfigurieren des Firefox-Browsers
 options = FirefoxOptions()
 options.add_argument("--headless")
 # driver = webdriver.Firefox()
@@ -17,102 +23,189 @@ driver = webdriver.Firefox(options=options)
 wait = WebDriverWait(driver, 20)
 first = True
 
+
 def next_page():
-	global end_reached
-	if duplicate_count <= 20  and end_reached is not True:
-
-		if len(driver.find_elements(By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right")) > 0:
-			driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right"))
+    global end_reached
+    if duplicate_count <= 20 and end_reached is not True:
+		# Überprüfen, ob es einen "Next"-Button gibt
+        if (
+            len(
+                driver.find_elements(
+                    By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right"
+                )
+            )
+            > 0
+        ):
+			# Scrollen, um den "Next"-Button sichtbar zu machen
+            driver.execute_script(
+                "arguments[0].scrollIntoView();",
+                driver.find_element(
+                    By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right"
+                ),
+            )
 		
-		if len(driver.find_elements(By.CSS_SELECTOR, ".jobletter.jobletterModal.show")) > 0:
-			driver.find_element(By.CSS_SELECTOR, ".simplemodal-close.icon.close").click()
+		# Überprüfen, ob ein Newsletter-Modal angezeigt wird
+        if (
+            len(driver.find_elements(By.CSS_SELECTOR, ".jobletter.jobletterModal.show"))
+            > 0
+        ):
+            # Schließe Newsletter-Modal
+            driver.find_element(
+                By.CSS_SELECTOR, ".simplemodal-close.icon.close"
+            ).click()
 
-		if len(driver.find_elements(By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right.disabled")) == 0:
-			driver.find_element(By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right").click()
-		else:
-			end_reached = True
+		# Überprüfen, ob "Next"-Button unverfügbar ist
+        if (
+            len(
+                driver.find_elements(
+                    By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right.disabled"
+                )
+            )
+            == 0
+        ):
+            # Klicke "Next"- Button
+            driver.find_element(
+                By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right"
+            ).click()
+        else:
+            end_reached = True
 
+# Lese bestehende Daten
 jobs_df = pd.read_pickle("../data/jobs.pkl")
 ix_jobs = jobs_df.shape[0]
 
 statistics = pd.read_pickle("../data/statistics.pkl")
 ix_stats = statistics.shape[0]
 
-# Open Search with keywords
-
+# Durchlaufen der Suchbegriffe
 for keywords in search_keywords:
-	new_entries = 0
-	duplicates_found = 0
-	duplicate_count = 0
-	end_reached = False
+    new_entries = 0
+    duplicates_found = 0
+    duplicate_count = 0
+    end_reached = False
 
-	driver.get(search_url+keywords)
+    driver.get(search_url + keywords)
+	# Überprüfen, ob es einen "Next"-Button gibt
+    if (
+        len(
+            driver.find_elements(
+                By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right"
+            )
+        )
+        > 0
+    ):
+        # Scrollen, um den "Next"-Button sichtbar zu machen
+        driver.execute_script(
+            "arguments[0].scrollIntoView();",
+            driver.find_element(
+                By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right"
+            ),
+        )
 
-	if len(driver.find_elements(By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right")) > 0:
-			driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.CSS_SELECTOR, ".next.icon.icon--right.chevron-right"))
-			# print("nach unten gescrollt")
+	# Beim ersten Durchlauf, Cookie Banner schließen
+    if first:
+        # Überprüfen, ob es einen Cookie-Banner gibt
+        wait.until(
+            lambda driver: len(
+                driver.find_elements(
+                    By.CSS_SELECTOR,
+                    "#ccmgt_explicit_preferences.privacy-prompt-button.secondary-button.options-button",
+                )
+            )
+            > 0
+        )
+        # Klicke ersten Teil des Cookie Banners
+        driver.find_element(
+            By.CSS_SELECTOR,
+            "#ccmgt_explicit_preferences.privacy-prompt-button.secondary-button.options-button",
+        ).click()
+        
+        # Klicke zweiten Teil des Cookie Banners
+        wait.until(
+            lambda driver: len(
+                driver.find_elements(
+                    By.CSS_SELECTOR,
+                    ".secondary-button.ccmgt_reject_button",
+                )
+            )
+            > 0
+        )
+        driver.find_element(
+            By.CSS_SELECTOR, ".secondary-button.ccmgt_reject_button"
+        ).click()
+        first = False
 
-	if first:
-		if len(driver.find_elements(By.CSS_SELECTOR, "#ccmgt_explicit_preferences.privacy-prompt-button.secondary-button.options-button")) > 0:
-				# print("cookie banner gefunden")
-				driver.find_element(By.CSS_SELECTOR, "#ccmgt_explicit_preferences.privacy-prompt-button.secondary-button.options-button").click()
-				# print("banner geklickt")
-				driver.find_element(By.CSS_SELECTOR, ".secondary-button.ccmgt_reject_button").click()
-				# print("2. banner geklickt")
-		first = False
-	
-	if len(driver.find_elements(By.CSS_SELECTOR, ".jobletter.jobletterModal.show")) > 0:
-		# print("newsletter modal")
-		driver.find_element(By.CSS_SELECTOR, ".simplemodal-close.icon.close").click()
-		# print("newsletter geschlossen")
+	# Wenn Newsletter-Modal, klicken
+    if len(driver.find_elements(By.CSS_SELECTOR, ".jobletter.jobletterModal.show")) > 0:
+        driver.find_element(By.CSS_SELECTOR, ".simplemodal-close.icon.close").click()
 
-	# while loop bis duplikat gefunden
-	while duplicate_count < 20 and end_reached is not True:
-		response = driver.page_source
-		soup_object = BeautifulSoup(response, features="lxml")
+    # Solange suchen, bis genug Duplikate gefunden oder Ende erreicht ist
+    while duplicate_count < 20 and end_reached is not True:
+        response = driver.page_source
+        soup_object = BeautifulSoup(response, features="lxml")
 
-		joblist_container = soup_object.find_all(id="joblist", class_="joblist copy-default")[0]
-		joblist = joblist_container.find_all("li", attrs={"data-hidesalarydata":True})
+        joblist_container = soup_object.find_all(
+            id="joblist", class_="joblist copy-default"
+        )[0]
+        joblist = joblist_container.find_all("li", attrs={"data-hidesalarydata": True})
 
-		for job in joblist:
-			link = job.find(class_= "jobListLink")["href"]
-			job_id = job.find(class_= "jobListLink")["href"].split("jobId=")[1]
-			titel = job.find(class_= "text title textlink-default").text
-			unternehmen = job.find(class_= "company").text
-			ort = job.find(class_= "location textlink-default").text
-			datum = datetime.datetime.now().strftime("%Y-%m-%d")
-			if job.find(class_= "salary month textlink-default") is not None:
-				gehalt_min = job.find(class_= "salary month textlink-default").text.split(" – ")[0].replace(".", "")
-				gehalt_max = job.find(class_= "salary month textlink-default").text.split(" – ")[1].replace(".", "")
-			else:
-				gehalt_min = ""
-				gehalt_max = ""
-			if ~jobs_df["JobID"].isin([job_id]).any():
-				jobs_df.loc[ix_jobs, ["Titel", 
-									"Unternehmen", 
-									"Ort", 
-									"Gehalt_min", 
-									"Gehalt_max", 
-									"JobID",
-									"Link", "Datum"]] = [titel, 
-														unternehmen,
-														ort, 
-														gehalt_min, 
-														gehalt_max,  
-														job_id, 
-														link, 
-														datum]
-				ix_jobs += 1
-				duplicate_count = 0
-				new_entries += 1
-			else:
-				duplicate_count += 1
-				duplicates_found += 1
+        for job in joblist:
+            link = job.find(class_="jobListLink")["href"]
+            job_id = job.find(class_="jobListLink")["href"].split("jobId=")[1]
+            titel = job.find(class_="text title textlink-default").text
+            unternehmen = job.find(class_="company").text
+            ort = job.find(class_="location textlink-default").text
+            datum = datetime.datetime.now().strftime("%Y-%m-%d")
+            if job.find(class_="salary month textlink-default") is not None:
+                gehalt_min = (
+                    job.find(class_="salary month textlink-default")
+                    .text.split(" – ")[0]
+                    .replace(".", "")
+                )
+                gehalt_max = (
+                    job.find(class_="salary month textlink-default")
+                    .text.split(" – ")[1]
+                    .replace(".", "")
+                )
+            else:
+                gehalt_min = ""
+                gehalt_max = ""
+            if ~jobs_df["JobID"].isin([job_id]).any():
+                jobs_df.loc[
+                    ix_jobs,
+                    [
+                        "Titel",
+                        "Unternehmen",
+                        "Ort",
+                        "Gehalt_min",
+                        "Gehalt_max",
+                        "JobID",
+                        "Link",
+                        "Datum",
+                    ],
+                ] = [
+                    titel,
+                    unternehmen,
+                    ort,
+                    gehalt_min,
+                    gehalt_max,
+                    job_id,
+                    link,
+                    datum,
+                ]
+                ix_jobs += 1
+                duplicate_count = 0
+                new_entries += 1
+            else:
+                duplicate_count += 1
+                duplicates_found += 1
 
-		next_page()
+        next_page()
 
-	
-	statistics.loc[ix_stats, ["Date", keywords]] = [datetime.datetime.now().strftime("%d.%m.%Y"), new_entries]
+    statistics.loc[ix_stats, ["Date", keywords]] = [
+        datetime.datetime.now().strftime("%d.%m.%Y"),
+        new_entries,
+    ]
 
 driver.quit()
 
