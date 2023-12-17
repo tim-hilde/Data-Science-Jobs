b4232a5df2f68e4843d86a79e67aa4d145002eb7
tim-hilde <tim.hildebrandt@me.com>
2023-12-17 11:04:46 +0100

    17.12.2023 Scraping

--- scripts/mining.zsh
+++ scripts/mining.zsh
@@ -5,13 +5,13 @@ set -e
 BASEDIR=$(dirname "$0")
 
 cd $BASEDIR
-/usr/local/bin/python3 "./scraping.py"
+/usr/local/bin/python3 "./scraping.py" &>> log.txt
 
-/usr/local/bin/python3 "./add_info.py"
+/usr/local/bin/python3 "./add_info.py" &>> log.txt
 
 cd ..
 
 DATUM=$(date +"%d.%m.%Y")
-git add . > /dev/null
-git commit -a -m "$DATUM Scraping" > /dev/null
-git push --quiet > /dev/null
+git add . &>> log.txt
+git commit -a -m "$DATUM Scraping" &>> log.txt
+git push --quiet &>> log.txt
